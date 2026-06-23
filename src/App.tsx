0fcaca98957f2deef3341072c6566041def96143
shawinsamuel184-cyber <shawinsamuel184@gmail.com>
2026-06-23 16:18:08 +0530

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Home from "@/pages/Home";
 
 export default function App() {
   return (
-    <Router>
+    <Router basename={import.meta.env.BASE_URL}>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/other" element={<div className="text-center text-xl">Other Page - Coming Soon</div>} />
