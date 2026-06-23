32712765bdb1592e39f104919a6cd88488aced8b
shawinsamuel184-cyber <shawinsamuel184@gmail.com>
2026-06-23 16:15:34 +0530

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "@/pages/Home";
+
+export default function App() {
+  return (
+    <Router basename={import.meta.env.BASE_URL}>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/other" element={<div className="text-center text-xl">Other Page - Coming Soon</div>} />
+      </Routes>
+    </Router>
+  );
+}
